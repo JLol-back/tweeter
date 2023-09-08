9480a4c56bb011846c95df542a0863a4764c734e
Jake Lollback <jalollback@gmail.com>
2023-09-07 21:45:40 -0500

    Refine styling of tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,41 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-
-
 $(document).ready(function() {
 
-
-const data = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+// Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 
 const createTweetElement = function(tweetData) {
-  const name = tweetData.user.name;
-  const avatar = tweetData.user.avatars;
-  const handle = tweetData.user.handle;
-  const content = tweetData.content;
-  const created_at = tweetData.created_at;
 
   const tweetHTML = `
         <article class="tweet">
           <header>
             <div>
-              <img width="80px" src="${tweetData.user.avatars}">
+              <img height="54px" src="${tweetData.user.avatars}">
               <h4>${tweetData.user.name}</h4>
             </div>
             <h4 class="handle">${tweetData.user.handle}</h4>
@@ -61,23 +62,15 @@ const createTweetElement = function(tweetData) {
   return tweetHTML;      
 }
 
-const $tweet = createTweetElement(data);
+const renderTweets = function(tweets) {
 
-console.log($tweet);
-
-$('.tweet-container').append($tweet);
-
-
-
-
-
-const renderTweets = function() {
+   tweets.forEach(tweet => {
+    let $tweet = createTweetElement(tweet);
+    $('.tweet-container').append($tweet);
+   });
 
 };
 
-
-
-
-
+renderTweets(data);
 
 });
