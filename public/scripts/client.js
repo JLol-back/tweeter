b5bc6cfeb0b96c82cc2380326d26c9b043c387f1
Jake Lollback <jalollback@gmail.com>
2023-09-08 18:31:18 -0500

    Implement timeago for tweet timestamps

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,35 +6,6 @@
 
 $(document).ready(function() {
 
-// Test / driver code (temporary). Eventually will get this from the server.
-/*
-
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-*/
-
 const createTweetElement = function(tweetData) {
 
   const tweetHTML = `
@@ -51,7 +22,7 @@ const createTweetElement = function(tweetData) {
             </div>
             <hr>
           <footer>
-           <p>${tweetData.created_at}</p>
+           <p>${timeago.format(tweetData.created_at)}</p>
            <div class="tweet-icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -60,7 +31,7 @@ const createTweetElement = function(tweetData) {
           </footer>
         </article>
         `;
-
+  
   return tweetHTML;      
 }
 
@@ -104,5 +75,4 @@ $( "form" ).on( "submit", function( event ) {
 
 
 // Close of document.ready function
-
 });
