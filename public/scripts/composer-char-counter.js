58081ab644a43d060806d6e028fd113aa66ac012
Jake Lollback <jalollback@gmail.com>
2023-09-15 17:37:33 -0500

    Refactored character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,42 +1,22 @@
 $(document).ready(function() {
   
-  $("#tweet-text").on("keyup", function(e) {
- 
-    //console.log(e.key);
-    //console.log(this);
-
+  $("#tweet-text").on("keyup", function (e) {
     textArea = $("#tweet-text");
-    //console.log("text:", textArea.val(), textArea.val().length);
 
     maxChar = 140;
-    
-    currentCharacters = $( this ).val().length;
 
-    if (e.key !== "Backspace") {
-      
+    currentCharacters = $(this).val().length;
+
     result = maxChar - currentCharacters;
-      $(this).next().children().eq(1).text(result);
+    $(this).next().children().eq(1).text(result);
 
+    if (result < 0) {
+      $(".counter").toggleClass("character-error", true);
     }
 
-    if ((e.key === "Backspace") && (result < 140)) {
-      
-    ++result  
-     $(this).next().children().eq(1).text(result);
- 
-     }
-
-     if (result < 0) {
-      $(".counter").toggleClass("character-error", true);
-     }
-
-     if (result > 0) {
+    if (result > 0) {
       $(".counter").toggleClass("character-error", false);
-     }
-
-  } )
+    }
+  });
 
 });
-
-
-
