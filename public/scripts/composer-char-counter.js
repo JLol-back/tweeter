9d4acd0b436edd064957478ab56256e44d2e1d29
Jake Lollback <jalollback@gmail.com>
2023-09-17 08:39:58 -0500

    Add comments for legibility

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
   
+  // Following each keyup event within the tweet form text area
+
   $("#tweet-text").on("keyup", function (e) {
     textArea = $("#tweet-text");
 
     maxChar = 140;
+    
+    // Update the count of characters currently within the text area
 
     currentCharacters = $(this).val().length;
 
     result = maxChar - currentCharacters;
+
+    // Target and update the text displaying the character count on the front end.
     $(this).next().children().eq(1).text(result);
 
+    // If character count is lower than zero or higher than the maximum, toggle on error class
+    
     if (result < 0) {
       $(".counter").toggleClass("character-error", true);
     }
